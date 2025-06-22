fac1611af4b5a0f0f9b17a379bceb6cc2b8664e4
shivaKotagiri <shivakotagiri532@gmail.com>
2025-06-22 20:18:56 +0530

    did some changes in navbar section

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,13 +5,13 @@ import Backtotop from "./backtotop";
 export default function() {
 
   return (
-    <div className="flex px-5 justify-between w-full text-xs md:text-sm">
+    <div className="flex px-7 md:px-10 lg:px-15 xl:px-25 justify-between w-full text-xs md:text-sm">
       <div className="font-semibold flex flex-col">
         <small>Design & Developed by</small>
         <small>ShivaKumarKotagiri</small>
       </div>
-      <div className="flex justify-center items-center">
-        <div className="flex justify-center text-center font-semibold text-xs gap-3 sm:text-base  md:gap-10">
+      <div className="flex justify-center items-center px-2">
+        <div className="flex justify-center text-center font-semibold text-xs gap-2 sm:gap-3 sm:text-base  md:gap-10">
           <Link target="_blank" href={"https://github.com/shivaKotagiri"} className="cursor-pointer transform transition ease-in-out duration-300 hover:text-[#ff6f00]">Github</Link>
           <Link target="_blank" href={"https://linkedin.com/in/kotagiri-shiva-670330288"} className="cursor-pointer transform transition ease-in-out duration-300 hover:text-[#ff6f00]">LinkedIn</Link>
           <Link target="_blank" href={"https://x.com/ShivaKumar403"} className="cursor-pointer transform transition ease-in-out duration-300 hover:text-[#ff6f00]">Twitter</Link>
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -11,21 +11,21 @@ export default function Navbar() {
   return (
     <div className={`fixed top-0 left-0 w-full px-10 py-3 border-gray-500 flex justify-between items-center z-10 transition-colors duration-500 ${lightMode ? "bg-[#FFFCF3] text-black" : "bg-black text-white"}`}>
       <div onClick={() => router.push("/#home")} className="cursor-pointer font-semibold text-2xl transform transition ease-in-out duration-200 hover:text-[#ff6f00]">Portfolio</div>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center gap-2 md:gap-4">
         <div
           onClick={toggleLightMode}
-          className={`cursor-pointer p-2 rounded-full`}
+          className={`cursor-pointer rounded-full`}
         >
-          {lightMode ? <MdDarkMode size={25} /> : <MdLightMode size={25} />}
+          {lightMode ? <MdDarkMode size={20} /> : <MdLightMode size={20} />}
         </div>
-        <div className="flex items-center">
-          <div onClick={() => router.push("/resume")} className={`cursor-pointer py-2 md:px-4 rounded-xl text-md transform transition ease-in-out duration-200 hover:text-[#ff6f00]`}>
+        <div className="flex items-center text-sm md:text-md gap-2 md:gap-4">
+          <div onClick={() => router.push("/resume")} className={`cursor-pointer rounded-xl transform transition ease-in-out duration-200 hover:text-[#ff6f00]`}>
             Resume
           </div>
-          <div className={`cursor-pointer text-md py-2 px-3 rounded-lg transform transition ease-in-out duration-200 hover:text-[#ff6f00]`}>
+          <div className={`cursor-pointer rounded-lg transform transition ease-in-out duration-200 hover:text-[#ff6f00]`}>
             About
           </div>
-          <div onClick={() => router.push("/#contact")} className="cursor-pointer text-md md:px-3 py-2 rounded-lg transform transition ease-in-out duration-200 hover:text-[#ff6f00]">
+          <div onClick={() => router.push("/#contact")} className="cursor-pointer rounded-lg transform transition ease-in-out duration-200 hover:text-[#ff6f00]">
             Contact
           </div>
         </div>
